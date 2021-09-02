c032881db17fc1903a5a3d47e11a3a77fa1606d0
yashaswini295558 <82228751+yashaswini295558@users.noreply.github.com>
2021-09-02 16:20:50 +0530

    Update Employee.cs

--- ProblemTwo_CompilationError/Employee.cs
+++ ProblemTwo_CompilationError/Employee.cs
@@ -6,29 +6,32 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class Employee
-    {
-        public int EmpID { get; set; }
-        public string EmpName { get; set; }
-        public int EmpLevel { get; set; }
-        private int NoOfLeaves { get; }
-        public DateTime DateOfJoining { get; set; }
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int EmpLevel { get; set; }
+        public int NoOfLeaves { get; set; }
+        public DateTime DateOfJoining { get; set; }
 
-        public Employee()
-        {
-            //Default number of leaves credited is 2 per month based on date of joining to the current date
-            NoOfLeaves= GetMonthDifference(DateOfJoining, DateTime.Now) * 2;
-        }
-        public Employee(int eID,string eName,int eLevel, DateTime eDoJ)
-        {
+        public Employee()
+        {
+            //Default number of leaves credited is 2 per month based on date of joining to the current date
+            NoOfLeaves = GetMonthDifference(DateOfJoining, DateTime.Now) * 2;
+        }
+        public Employee(int eID, string eName, int eLevel, DateTime eDoJ)
+        {
+            this.EmpID = eID;
+            this.EmpName = eName;
+            this.EmpLevel = eLevel;
+            this.DateOfJoining = eDoJ;
+        }
 
-        }
+        public static int GetMonthDifference(DateTime startDate, DateTime endDate)
+        {
+            int monthsApart = 12 * (startDate.Year - endDate.Year) + startDate.Month - endDate.Month;
+            return Math.Abs(monthsApart);
+        }
 
-        public static int GetMonthDifference(DateTime startDate, DateTime endDate)
-        {
-            int monthsApart = 12 * (startDate.Year - endDate.Year) + startDate.Month - endDate.Month;
-            return Math.Abs(monthsApart);
-        }
-
-    }
+    }
 }
