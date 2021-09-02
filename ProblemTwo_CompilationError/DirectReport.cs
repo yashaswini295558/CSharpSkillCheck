c054126e841a11c65a634ea8d6e2d446ac10f19a
yashaswini295558 <82228751+yashaswini295558@users.noreply.github.com>
2021-09-02 16:21:49 +0530

    Update DirectReport.cs

--- ProblemTwo_CompilationError/DirectReport.cs
+++ ProblemTwo_CompilationError/DirectReport.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class DirectReport
-    {
-        public Employee ReportsTo { get; set; }
-        public string ProjectCode { get; set; }
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int EmpLevel { get; set; }
+        public int NoOfLeaves { get; set; }
+        public DateTime DateOfJoining { get; set; }
 
-        public DirectReport(Employee managerEmpObj,string pCode) :base(eID,eName,eLevel,eDoJ)
-        {
-            ReportsTo = managerEmpObj;
-            ProjectCode = pCode;
-        }
-    }
+        public Employee()
+        {
+            //Default number of leaves credited is 2 per month based on date of joining to the current date
+            NoOfLeaves = GetMonthDifference(DateOfJoining, DateTime.Now) * 2;
+        }
+        public Employee(int eID, string eName, int eLevel, DateTime eDoJ)
+        {
+            this.EmpID = eID;
+            this.EmpName = eName;
+            this.EmpLevel = eLevel;
+            this.DateOfJoining = eDoJ;
+        }
+
+        public static int GetMonthDifference(DateTime startDate, DateTime endDate)
+        {
+            int monthsApart = 12 * (startDate.Year - endDate.Year) + startDate.Month - endDate.Month;
+            return Math.Abs(monthsApart);
+        }
+
+    }
 }
